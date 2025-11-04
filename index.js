932e5db88a64c2fa6c1ebc229c38a6c67e7d7a87
leclavierfrancais <le.clavier.francais@gmail.com>
2025-11-04 10:16:38 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ import { extension_settings, getContext, loadExtensionSettings } from "../../../
 //You'll likely need to import some other functions from the main script
 import { saveSettingsDebounced } from "../../../../script.js";
 
-// Keep track of where your extension is located, name should match repo name
-const extensionName = "st-extension-example-master";
+// Keep track of where your extension is located, name should match the manifest name
+const extensionName = "user-expressions";
 const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
 const extensionSettings = extension_settings[extensionName];
 const defaultSettings = {
